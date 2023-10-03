6c7ee79339ec9669b72f2e8beaed2c280f17fda8
LinhDancute <dannguyen11003@gmail.com>
2023-10-03 16:06:32 +0700

    Update Sendcode and VerifyCode

--- Areas/Identity/Models/Account/SendCodeViewModel.cs
+++ Areas/Identity/Models/Account/SendCodeViewModel.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,6 +12,7 @@ namespace App.Areas.Identity.Models.AccountViewModels
 {
     public class SendCodeViewModel
     {
+        [Required(ErrorMessage = "The Providers field is required.")]
         public string SelectedProvider { get; set; }
 
         public ICollection<SelectListItem> Providers { get; set; }
